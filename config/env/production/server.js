28aa98c51d769d5ffb214269b4cf78138c05967e
Krane96 <69842760+Krane96@users.noreply.github.com>
2022-03-09 13:44:34 +0100

    test

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,3 +1,7 @@
 module.exports = ({ env }) => ({
     url: env('https://hotelstrapimain.herokuapp.com/'),
-    proxy: true,app: {keys: env.array('APP_KEYS', ['key1', 'key2']),  },});
+    proxy: true,
+    app: {
+        keys: env.array('APP_KEYS', ['key1', 'key2']),
+  },
+});
